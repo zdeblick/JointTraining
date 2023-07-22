db7b26cdfe9390c1835ee5fa2d703cf0dc24b13c
zdeblick <daniel.zdeblick@hpc-login.corp.alleninstitute.org>
2023-07-22 16:06:40 -0700

    lrdecay, L=1,10000 eps

--- YL_sum_script.py
+++ YL_sum_script.py
@@ -4,22 +4,20 @@ import os
 os.chdir('YL_results')
 savepath = '../YL_summaries/'
 
-L=4
+L=1
 inds = 20000
-epochs = 200
-D0 = np.load('YL_LBFGS_id=0_neps='+str(epochs)+'_L='+str(L)+'.npz')
+eps = [10000]#,5000,10000]
+D0 = np.load('YLdec_id=0_neps='+str(eps[0])+'_L='+str(L)+'.npz')
 keys = D0.keys()
-big_D = {k+'s':np.nan*np.ones(np.hstack((inds,D0[k].shape)).astype(int) ) for k in keys}
+big_D = {k+'s':np.nan*np.ones((inds,len(eps),D0[k].size)) for k in keys}
 for id in range(inds):
-    try:
-        D = np.load('YL_LBFGS_id='+str(id)+'_neps='+str(epochs)+'_L='+str(L)+'.npz')
-        for k in keys:
-            big_D[k+'s'][id] = D[k]
-    except Exception as e:
-        print(e)
-
-eps = (0,1,4,9,19,49,99,199)
+    for ne, epochs in enumerate(eps):
+        try:
+            D = np.load('YLdec_id='+str(id)+'_neps='+str(epochs)+'_L='+str(L)+'.npz')
+            for k in keys:
+                big_D[k+'s'][id,ne,:] = D[k]
+        except Exception as e:
+            print(e)
 big_D = {k:np.squeeze(v) for k,v in big_D.items()}
-big_D['Cvalss'] = big_D['Cvalss'][:,:,eps]
-big_D['eps'] = eps
+big_D['epochs'] = eps
 np.savez(savepath+'YL_sum_varySNMQ_L='+str(L),**big_D)
