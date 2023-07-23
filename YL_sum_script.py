96a9892b7f50b89bf7d144cffafbfe6f3743abc4
zdeblick <daniel.zdeblick@hpc-login.corp.alleninstitute.org>
2023-07-22 17:39:47 -0700

    shouldnt work

--- YL_sum_script.py
+++ YL_sum_script.py
@@ -6,7 +6,7 @@ savepath = '../YL_summaries/'
 
 L=1
 inds = 975
-eps = [10000]#,5000,10000]
+eps = [1000,5000,10000]
 D0 = np.load('YL_id=0_neps='+str(eps[0])+'_L='+str(L)+'.npz')
 keys = D0.keys()
 big_D = {k+'s':np.nan*np.ones((inds,len(eps),D0[k].size)) for k in keys}
